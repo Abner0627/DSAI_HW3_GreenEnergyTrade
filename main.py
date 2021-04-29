273bdb08f21720f6df80f3853ccd5cb1af6b8f1c
Abner0627 <n16090065@gs.ncku.edu.tw>
2021-04-29 15:38:34 +0800

    4/29

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ def config():
     parser.add_argument("--consumption", default="./sample_data/consumption.csv", help="input the consumption data path")
     parser.add_argument("--generation", default="./sample_data/generation.csv", help="input the generation data path")
     parser.add_argument("--bidresult", default="./sample_data/bidresult.csv", help="input the bids result path")
-    parser.add_argument("--train", default=True, help="training model or not")
+    parser.add_argument("--train", default=False, help="training model or not")
+    parser.add_argument("--valid", default=True, help="training model or not")
     parser.add_argument("--output", default="output.csv", help="output the bids path")
 
     return parser.parse_args()
@@ -19,9 +20,8 @@ import numpy as np
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
-import sklearn
+import joblib
 from sklearn import linear_model
-import random
 import func
 
 
@@ -48,12 +48,23 @@ if args.train:
     Cndata, Cmu, Cstd = func._nor(Cdata, train=True)
 
     Gmodel = linear_model.Lasso(alpha=1e-2, positive=True)
-    Cmodel = linear_model.ElasticNet(alpha=1e-4, positive=True)
+    Cmodel = linear_model.ElasticNet(alpha=1e-4, positive=True) 
     Gmodel.fit(Gndata, Glabel)
     Cmodel.fit(Cndata, Clabel)
+    joblib.dump(Gmodel, 'Gmodel')
+    joblib.dump(Cmodel, 'Cmodel')
 
-
+elif args.valid:
     #%% val pred
+    Gmodel = joblib.load('Gmodel')
+    Cmodel = joblib.load('Cmodel')
+
+    with open('nor.npy', 'rb') as f:
+        Gmu = np.load(f)
+        Gstd = np.load(f)
+        Cmu = np.load(f)
+        Cstd = np.load(f)
+    
     Cp = './sample_data/consumption_org.csv'
     Gp = './sample_data/generation_org.csv'
     GVal = np.array(pd.read_csv(Gp, header=None))[1:,1:]
@@ -69,13 +80,19 @@ if args.train:
     Gpred = Gmodel.predict(GnVdata)
     Cpred = Cmodel.predict(CnVdata)
 
+    with open('nor.npy', 'wb') as f:          
+        np.save(f, Gmu)
+        np.save(f, Gstd)
+        np.save(f, Cmu)
+        np.save(f, Cstd)              
+
     #%%
-    # fig, ax = plt.subplots(1, 1, figsize = (15,5))
-    # ax.plot(Gpred, color='dodgerblue', label='Pred')
-    # ax.plot(GVlabel, color='darkorange', label='Label')
-    # ax.legend(fontsize=10, loc=4)
-    # plt.title('Generation', fontsize=30) 
-    # plt.tight_layout()
+    fig, ax = plt.subplots(1, 1, figsize = (15,5))
+    ax.plot(Gpred, color='dodgerblue', label='Pred')
+    ax.plot(GVlabel, color='darkorange', label='Label')
+    ax.legend(fontsize=10, loc=4)
+    plt.title('Generation', fontsize=30) 
+    plt.tight_layout()
 
     fig, ax = plt.subplots(1, 1, figsize = (15,5))
     ax.plot(Cpred, color='dodgerblue', label='Pred')
@@ -83,7 +100,31 @@ if args.train:
     ax.legend(fontsize=10, loc=4)
     plt.title('Consumption', fontsize=30) 
     plt.tight_layout()
-    plt.show()    
+    plt.show()
+
+else:
+    #%% val pred
+    Gmodel = joblib.load('Gmodel')
+    Cmodel = joblib.load('Cmodel')
+
+    with open('nor.npy', 'rb') as f:
+        Gmu = np.load(f)
+        Gstd = np.load(f)
+        Cmu = np.load(f)
+        Cstd = np.load(f)
+
+    GVal = np.array(pd.read_csv(args.generation, header=None))[1:8,1:]
+    GVal = np.stack(GVal).astype(None)[:,0]
+    GVal = GVal[np.newaxis, :]
+    CVal = np.array(pd.read_csv(args.consumption, header=None))[1:8,1:]
+    CVal = np.stack(CVal).astype(None)[:,0]   
+    CVal = CVal[np.newaxis, :]
+
+    GnVdata, _, _ = func._nor(GVal, Gmu, Gstd)
+    CnVdata, _, _ = func._nor(CVal, Cmu, Cstd)
+
+    Gpred = Gmodel.predict(GnVdata)
+    Cpred = Cmodel.predict(CnVdata)      
    
 
     
