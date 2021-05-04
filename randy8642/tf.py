66758f752ad1918cfbb08af24ea65fe9d6229f6c
randy8642 <randy8642@gmail.com>
2021-05-04 20:24:57 +0800

    20210504

--- randy8642/tf.py
+++ randy8642/tf.py
@@ -1,10 +1,12 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import os
+
 
 #################################################################################
 # DATA
@@ -12,8 +14,8 @@ import os
 
 def train_data(name: str):
 
-    train_x = np.zeros([0, 7*24, 1])
-    train_y = np.zeros([0, 1])
+    train_x = np.zeros([0, 24, 7])
+    train_y = np.zeros([0, 24, 1])
 
     training_data_path = './training_data'
     for dirPath, dirNames, fileNames in os.walk(training_data_path):
@@ -24,12 +26,12 @@ def train_data(name: str):
             length = len(df.index)
             data = df[name].values
 
-            train_x_target = np.zeros([length - 7*24, 7*24, 1])
-            train_y_target = np.zeros([length - 7*24, 1*24])
+            train_x_target = np.zeros([length - 8*24 + 1, 24, 7])
+            train_y_target = np.zeros([length - 8*24 + 1, 24, 1])
 
-            for i in range(length - 7):
-                train_x_target[i, :, 0] = data[i:i+6*24]
-                train_y_target[i, :] = data[i+6*24:i+7*24]
+            for i in range(length - 8*24 + 1):
+                train_x_target[i, :, :] = data[i:i+7*24].reshape(1, 24, 7)
+                train_y_target[i, :, :] = data[i+7*24:i+8*24].reshape(1, 24, 1)
 
 
             train_x = np.concatenate((train_x, train_x_target), axis=0)
@@ -47,41 +49,28 @@ def test_data(name: str):
     data = df[name].values
     length = len(df.index)
 
-    test_x = np.zeros([length - 7, 7, 1])
-    test_y = np.zeros([length - 7, 1])
+    test_x = np.zeros([length - 8*24 + 1, 24, 7])
+    test_y = np.zeros([length - 8*24 + 1, 24, 7])
 
-    for i in range(length - 7):
-        test_x[i, :, 0] = data[i:i+7]
-        test_y[i, :] = data[i+7]
+    for i in range(length - 8*24 + 1):
+        test_x[i, :, :] = data[i:i+7*24].reshape(1, 24, 7)
+        test_y[i, :, :] = data[i+7*24:i+8*24].reshape(1, 24, 1)
 
     return test_x, test_y
 
 
-data = {}
-for stage in ['train', 'test']:
-    data[stage] = {}
-    for itemName in ['generation', 'consumption']:
-        data[stage][itemName] = {}
-        for label in ['x', 'y']:
-            data[stage][itemName][label] = None
-
 name = 'consumption'
 
-x, y = train_data(name)
-data['train'][name]['x'] = x
-data['train'][name]['y'] = y
-del x, y
-x, y = test_data(name)
-data['test'][name]['x'] = x
-data['test'][name]['y'] = y
-del x, y
+train_x, train_y = train_data(name)
+test_x, test_y = test_data(name)
+
 
 #################################################################################
 # MODEL
 
 model = keras.Sequential()
 
-model.add(layers.Bidirectional(layers.LSTM(256, return_sequences=False), merge_mode='concat'))
+model.add(layers.Bidirectional(layers.LSTM(256, return_sequences=True), merge_mode='concat'))
 model.add(layers.Dense(12))
 model.add(layers.Dense(1))
 
@@ -100,26 +89,25 @@ model.compile(
 
 
 model.fit(
-    data['train'][name]['x'], data['train'][name]['y'], batch_size=512, epochs=10, verbose=1, shuffle=False
+    train_x, train_y, batch_size=512, epochs=10, verbose=1, shuffle=True
 )
   
 
 #################################################################################
 # TEST
 model.reset_states()
-model.predict(data['train'][name]['x'], batch_size=512)
 
 predictions = []
 expecteds = []
-for i in range(len(data['test'][name]['x'])):
+for i in range(len('')):
     # make one-step forecast
-    X = data['test'][name]['x'][i]
-    X = X.reshape(1, 7, 1)
+    X = test_x[i]
+    X = X.reshape(1, 7, 24)
     yhat = model.predict(X)[0,0]
     
     # store forecast
     predictions.append(yhat)
-    expecteds.append(data['test'][name]['y'][i,0])
+    expecteds.append(test_y[i,0])
     
 
 
